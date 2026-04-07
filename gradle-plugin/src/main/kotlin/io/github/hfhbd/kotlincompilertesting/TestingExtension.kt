8134f08b5d62a4f002a5a8b0654887cc7b28fd5c
hfhbd <22521688+hfhbd@users.noreply.github.com>
2026-04-07 13:35:44 +0200

    WIP
    # Conflicts:
    #	gradle/wrapper/gradle-wrapper.jar
    #	gradle/wrapper/gradle-wrapper.properties

---
+++ gradle-plugin/src/main/kotlin/io/github/hfhbd/kotlincompilertesting/TestingExtension.kt
@@ -0,0 +1,25 @@
+package io.github.hfhbd.kotlincompilertesting
+
+import org.gradle.api.Action
+import org.gradle.api.artifacts.dsl.Dependencies
+import org.gradle.api.artifacts.dsl.DependencyCollector
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Nested
+
+interface TestingExtension {
+    val mainClass: Property<String>
+
+    @get:Nested
+    val dependencies: TestingDependencies
+
+    fun dependencies(action: Action<TestingDependencies>) {
+        action.execute(dependencies)
+    }
+}
+
+interface TestingDependencies : Dependencies {
+    val annotation: DependencyCollector
+    val testCompiler: DependencyCollector
+
+    fun kotlin(version: String) = dependencyFactory.create("$GROUP:kotlin:$VERSION")
+}
